0460f68a77c8df9493cedc5747365ed6ff8afddd
jeinieldjs <silvestrejeiniel@gmail.com>
2024-06-10 18:14:01 +0800

    added functions to fetch data'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {Component} from "react";
 import Nav from "./components/Nav";
 import SearchBar from "./components/SearchBar";
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <SearchBar />
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      searchTerm: "",
+      apiKey: process.env.REACT_APP_API,
+    };
+  }
 
+  handleSubmit = () => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${this.state.apiKey}&query=${this.state.searchTerm}`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ movies: [...data.results] });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <SearchBar handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
     return (
         <div className="containter">
             <div className="row">
                 <section className="col s4 offset-s4">
-                    <form action="">
+                    <form action="" onSubmit = {props.handleSubmit}>
                         <div className="input-field">
-                            <input type="text" placeholder="Find a Flick"></input> 
+                            <input type="text" placeholder="Find a Flick" onChange={props.handleChange}></input> 
                         </div>
                     </form>
                 </section>
