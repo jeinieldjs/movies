a9495da37c97f7dab74c90f4717a367158ce477b
jeinieldjs <silvestrejeiniel@gmail.com>
2024-06-12 10:31:48 +0800

    Added prev/next on pagebar

--- src/components/PageBar.jsx
+++ src/components/PageBar.jsx
@@ -12,7 +12,9 @@ const PageBar = (props) => {
         <div className="container">
             <div className="row">
                 <ul className="pagination">
+                    { props.currentPage > 1 ? <li className={`waves-effect`} onClick={() => props.nextPage(props.currentPage - 1)}><a href="#">Prev</a></li> : '' }
                     { pageLinks }
+                    { props.currentPage < props.pages + 1 ? <li className={`waves-effect`} onClick={() => props.nextPage(props.currentPage + 1)}><a href="#">Next</a></li> : '' }
                 </ul>
             </div>
         </div>
