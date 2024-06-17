78f1244d22e55aa25752ed88107b180020e62151
jeinieldjs <silvestrejeiniel@gmail.com>
2024-06-17 14:29:19 +0800

    changed fornt family on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
     return (
         <nav>
             <div className="nav-wrapper container">
-                <a href="#" className="brand-logo">Flicktionary</a>
+                <a href="#" className="brand-logo" style={{fontFamily: "'Playwrite US Modern', cursive", fontWeight: 800}}>FLICTIONARY</a>
             </div>
         </nav>
     )
